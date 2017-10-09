70e84a3a1bed51e65ad6aa9bc7c304e43aa237f6
castarox <mika.kamil12@gmail.com>
2017-10-09 09:28:42 +0200

    Create empty project

---
+++ src/main/java/com/example/dispringguru/DiSpringGuruApplication.java
@@ -0,0 +1,12 @@
+package com.example.dispringguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiSpringGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiSpringGuruApplication.class, args);
+	}
+}
